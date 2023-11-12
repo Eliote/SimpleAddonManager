3fc7a15fbc1361d3f5cc9154487cb77390d93880
Eliote <eliote@eliote.dev>
2023-11-11 21:09:33 -0300

    Fix compat with WotLK
    Prioritize C_AddOns function

--- CoreCompat.lua
+++ CoreCompat.lua
@@ -3,27 +3,25 @@ local _, T = ...
 --- @type SimpleAddonManager
 local frame = T.AddonFrame
 
-frame.compat = {
-	EnableAddOn = EnableAddOn or C_AddOns.EnableAddOn,
-	DisableAddOn = DisableAddOn or C_AddOns.DisableAddOn,
-	IsAddOnLoaded = IsAddOnLoaded or C_AddOns.IsAddOnLoaded,
-	EnableAllAddOns = EnableAllAddOns or C_AddOns.EnableAllAddOns,
-	DisableAllAddOns = DisableAllAddOns or C_AddOns.DisableAllAddOns,
-	GetAddOnInfo = GetAddOnInfo or C_AddOns.GetAddOnInfo,
-	GetAddOnDependencies = GetAddOnDependencies or C_AddOns.GetAddOnDependencies,
-	GetNumAddOns = GetNumAddOns or C_AddOns.GetNumAddOns,
-	SaveAddOns = SaveAddOns or C_AddOns.SaveAddOns,
-	ResetAddOns = ResetAddOns or C_AddOns.ResetAddOns,
-	IsAddonVersionCheckEnabled = IsAddonVersionCheckEnabled or C_AddOns.IsAddonVersionCheckEnabled,
-	SetAddonVersionCheck = SetAddonVersionCheck or C_AddOns.SetAddonVersionCheck,
-	IsAddOnLoadOnDemand = IsAddOnLoadOnDemand or C_AddOns.IsAddOnLoadOnDemand,
-}
-
-local GetAddOnEnableState
-if (GetAddOnEnableState ~= nil) then
-	frame.compat.GetAddOnEnableState = function(nameOrIndex, character)
-		GetAddOnEnableState(character, nameOrIndex) -- the old API has inverted parameters
-	end
-else
-	frame.compat.GetAddOnEnableState = C_AddOns.GetAddOnEnableState
+local function getFunction(fun)
+	return (C_AddOns and C_AddOns[fun]) or _G[fun]
 end
+
+frame.compat = {
+	EnableAddOn = getFunction("EnableAddOn"),
+	DisableAddOn = getFunction("DisableAddOn"),
+	IsAddOnLoaded = getFunction("IsAddOnLoaded"),
+	EnableAllAddOns = getFunction("EnableAllAddOns"),
+	DisableAllAddOns = getFunction("DisableAllAddOns"),
+	GetAddOnInfo = getFunction("GetAddOnInfo"),
+	GetAddOnDependencies = getFunction("GetAddOnDependencies"),
+	GetNumAddOns = getFunction("GetNumAddOns"),
+	SaveAddOns = getFunction("SaveAddOns"),
+	ResetAddOns = getFunction("ResetAddOns"),
+	IsAddonVersionCheckEnabled = getFunction("IsAddonVersionCheckEnabled"),
+	SetAddonVersionCheck = getFunction("SetAddonVersionCheck"),
+	IsAddOnLoadOnDemand = getFunction("IsAddOnLoadOnDemand"),
+	GetAddOnEnableState = (C_AddOns and C_AddOns.GetAddOnEnableState) or function(nameOrIndex, character)
+		return GetAddOnEnableState(character, nameOrIndex) -- the old API has inverted parameters
+	end
+}
